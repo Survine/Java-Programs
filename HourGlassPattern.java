12a78b82628cf9df49f53c69f04b6fb11a6cbcec
Survine <hridaybardhan18@gmail.com>
2024-07-20 05:26:04 +0530

    gWy

--- HourGlassPattern.java
+++ HourGlassPattern.java
@@ -1,3 +1,27 @@
 public class HourGlassPattern {
-    
+    public static void main(String[] args) {
+        printHourglass(5);
+    }
+
+    public static void printHourglass(int size) {
+        for(int i=0; i<size; i++) {
+            for(int j=0; j<i+1; j++) {
+                System.out.print(" ");
+            }
+            for(int k=0; k<size-i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=1; i<size; i++) {
+            for(int j=0; j<size-i; j++) {
+                System.out.print(" ");
+            }
+            for(int k=0; k<i+1; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
 }
